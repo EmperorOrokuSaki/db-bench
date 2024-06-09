c3317337b924d7758e80e833fb13b1a204d44dd4
EmperorOrokuSaki <artie.eth@gmail.com>
2024-06-09 19:07:40 +0400

    feat: pretty size output and cleanup

--- benches/sled.rs
+++ benches/sled.rs
@@ -2,7 +2,13 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use db_bench::*;
 
 fn sled_benchmarks(c: &mut Criterion) {
-    c.bench_function("initialize", |b| b.iter(|| initialize_sled()));
+    c.bench_function("SLED DB COLD INITIALIZATION", |b| {
+        b.iter(|| initialize_sled())
+    });
+    println!(
+        "SLED DB SIZE AFTER COLD INITIALIZATION: {}",
+        measure_sled_size()
+    );
 }
 
 criterion_group!(benches, sled_benchmarks);
--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,17 @@ pub fn initialize_sled() {
     sled::open(SLED_PATH).unwrap();
 }
 
-pub fn measure_sled_size() -> u64 {
+pub fn add_to_sled() {}
+
+pub fn measure_sled_size() -> String {
     calculate_directory_size(SLED_PATH)
 }
 
-pub fn measure_rocksdb_size() -> u64 {
+pub fn measure_rocksdb_size() -> String {
     calculate_directory_size(ROCKS_DB_PATH)
 }
 
-fn calculate_directory_size(path: &str) -> u64 {
+fn calculate_directory_size(path: &str) -> String {
     let mut total_size = 0;
 
     // Traverse the directory and its subdirectories
@@ -31,5 +33,14 @@ fn calculate_directory_size(path: &str) -> u64 {
         }
     }
 
-    total_size
+    let units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"];
+    let mut size = total_size as f64;
+    let mut unit = 0;
+
+    while size >= 1024.0 && unit < units.len() - 1 {
+        size /= 1024.0;
+        unit += 1;
+    }
+
+    format!("{:.2} {}", size, units[unit])
 }
